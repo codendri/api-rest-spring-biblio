aed92108d733d6bf18a6871cba0db1a747ccfdd1
kloglossi <luis>
2025-06-20 13:44:24 +0000

    first commit

---
+++ src/main/java/com/example/techbiblio/TechBiblioApplication.java
@@ -0,0 +1,13 @@
+package com.example.techbiblio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechBiblioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechBiblioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techbiblio/TechBiblioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techbiblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBiblioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
